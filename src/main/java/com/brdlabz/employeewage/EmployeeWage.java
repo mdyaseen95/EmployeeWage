df884ec6f08fa20a268d82524880fd7ee34580e0
mdyaseen95 <yaseen.koppal@gmail.com>
2021-09-13 11:48:53 +0530

    ADD Welcome to Employee Wage calculation Programm

---
+++ src/main/java/com/brdlabz/employeewage/EmployeeWage.java
@@ -0,0 +1,7 @@
+package com.brdlabz.employeewage;
+
+public class EmployeeWage {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Employee Wage Calculation Program");
+    }
+}
