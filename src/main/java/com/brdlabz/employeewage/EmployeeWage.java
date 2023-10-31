bb835e5f84a82e51a42715b647b7777ff5918db8
mdyaseen95 <89438506+mdyaseen95@users.noreply.github.com>
2023-10-31 14:41:52 +0530

    Update EmployeeWage.java

--- src/main/java/com/brdlabz/employeewage/EmployeeWage.java
+++ src/main/java/com/brdlabz/employeewage/EmployeeWage.java
@@ -3,5 +3,6 @@ package com.brdlabz.employeewage;
 public class EmployeeWage {
     public static void main(String[] args) {
         System.out.println("Welcome to Employee Wage Calculation Program");
+        System.out.println("Fareed");
     }
 }
